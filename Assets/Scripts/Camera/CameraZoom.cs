68028ec79b0f85fe3abccfd97c5dbe2bfc5e9d06
Jonathan Porta <jonathan@jonathanPorta.com>
2015-06-29 07:08:05 -0700

    Add programatic splatmap example

---
+++ Assets/Scripts/Camera/CameraZoom.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraZoom : MonoBehaviour {
+	
+	public float zoomSpeed = 100.0f;
+
+	private Vector3 moveDirection;
+
+	void Start(){
+		this.moveDirection = Vector3.zero;
+	}
+
+	void Update(){
+		moveDirection = Vector3.forward * Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+
+		if(Input.GetAxis("Mouse ScrollWheel") != 0){
+			transform.Translate(moveDirection, Space.Self);
+		}
+	}
+}
